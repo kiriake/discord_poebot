935316d8549abe4783004ad603b000a8520935c2
Koji Kiriake <kiriake@gmail.com>
2021-02-16 17:17:59 +0900

    Fix typo

--- discord_poebot/discord_poebot.py
+++ discord_poebot/discord_poebot.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-client discord.Client()
+client = discord.Client()
 
 @client.event
 async def on_ready():
