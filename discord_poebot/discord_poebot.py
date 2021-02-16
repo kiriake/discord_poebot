6d4cb197e66077b925238a8998c608c53b677bdd
Koji Kiriake <kiriake@gmail.com>
2021-02-16 16:46:40 +0900

    Add bot function

--- discord_poebot/discord_poebot.py
+++ discord_poebot/discord_poebot.py
@@ -1,3 +1,36 @@
-def main():
+import os
+import logging
+import requests
 
-    print('main process')
+import discord
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+client discord.Client()
+
+@client.event
+async def on_ready():
+    logging.info('discord_poebot ready. ')
+
+@client.event
+async def on_message(message):
+    if message.author == client.bot:
+        return
+
+    if message.author == client.user:
+        logging.info(message)
+
+@client.event
+async def reply(message):
+    if message.author == client.user:
+        logging.info(message)
+        await message.channel.send('Reforges a rare item with new random modifiers. ')
+
+try:
+    client.run(os.environ(['DISCORD_TOKEN']))
+except KeyError as err:
+    logging.error(err)
