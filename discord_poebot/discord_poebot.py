f5cd36e3fcee90d8d30306502df4f816a701696b
Koji Kiriake <kiriake@gmail.com>
2021-02-25 17:03:15 +0000

    Fix classification

--- discord_poebot/discord_poebot.py
+++ discord_poebot/discord_poebot.py
@@ -1,7 +1,6 @@
 import os
 import logging
 import requests
-
 import discord
 
 
@@ -10,33 +9,39 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-client = discord.Client()
+TOKEN = os.environ['DISCORD_TOKEN']
 
-@client.event
-async def reply(message):
-    if message.author == client.user:
-        reply = '{message.author.mention} Reforges a rare item with new random modifiers. '
-        await message.channel.send(reply)
 
-@client.event
-async def on_ready():
-    logging.info('discord_poebot ready. ')
+class Discord_poebot(discord.Client):
 
-@client.event
-async def on_message(message):
-    if message.author.bot or message.channel.id != 810816487044546591:
-        return
 
-    if message.author == client.user:
-        logging.info(message)
+    # @client.event
+    async def on_ready(self):
+        logging.info('discord_poebot ready. ')
 
-    if client.user in message.mentions:
-        await reply(message)
+    # @client.event
+    async def reply(self, message):
+        if message.author == self.user:
+            reply = '{message.author.mention} Reforges a rare item with new random modifiers. '
+            await message.channel.send(reply)
 
-    if message.content == '/ping':
-        await message.channel.send('pong. ')
+    # @client.event
+    async def on_message(self, message):
+        if message.author.bot or message.channel.id != 810816487044546591:
+            return
 
+        if message.author == self.user:
+            logging.info(message)
+
+        if self.user in message.mentions:
+            await self.reply(message)
+
+        if message.content == '/ping':
+            await message.channel.send('pong. ')
+
+client = Discord_poebot()
 try:
-    client.run(os.environ['DISCORD_TOKEN'])
+    client.run(TOKEN)
 except KeyError as err:
     logging.error(err)
+
